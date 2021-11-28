2346be32f7f648c4e2a4d35d0c20791e9f501b8f
EC2 Default User <s.marika441@gmail.com>
2021-11-28 11:48:06 +0000

    [Start]応用-3

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/home/about" => "homes#about"
 
-  resources :books
+  resources :books do
+  resource :favorites, only: [:create, :destroy]
+  end
+  
   resources :users
 
 end
