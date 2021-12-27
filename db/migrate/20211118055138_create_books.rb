dd3653dcbfc6c7b80c5e360f866e7af337ec8537
EC2 Default User <s.marika441@gmail.com>
2021-12-27 12:24:43 +0000

    [Add]ユーザー詳細画面にカレンダー表示

--- db/migrate/20211118055138_create_books.rb
+++ db/migrate/20211118055138_create_books.rb
@@ -3,6 +3,8 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.text :title
       t.text :body
+      # カレンダーに出力させる
+      t.datetime :start_time
       t.integer :user_id
 
       t.timestamps
